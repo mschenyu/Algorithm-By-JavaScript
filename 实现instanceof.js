2995d3da34af91836f9aa5fc2091840decdc0fdc
chenyu <18220176924@163.com>
2020-09-03 18:50:49 +0800

    实现instanceof

---
+++ 实现instanceof.js
@@ -0,0 +1,17 @@
+// instanceof主要的实现原理就是验证右边变量的prototype是否在左边变量的原型链上，因此，instanceof在查找的过程中会遍历左边变量的原型链，直到找到右边变量的prototype，如果查找失败，则返回false
+
+function new_instanceof(leftValue, rightValue){
+  const rightPrototype = rightValue.prototype;
+  leftValue = leftValue.__proto__;
+  while(true){
+    if(leftValue === null){
+      return false;
+    }
+    if(leftValue === rightPrototype){
+      return true;
+    }
+    leftValue = leftValue.__proto__;
+  }
+}
+
+console.log(new_instanceof(Function,Object))
