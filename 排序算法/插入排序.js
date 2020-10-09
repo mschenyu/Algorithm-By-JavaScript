cfd6064a48c895ff212c32f3fe5ffd86a354555f
chenyu <18220176924@163.com>
2020-10-09 22:27:21 +0800

    insert sort

--- 排序算法/插入排序.js
+++ 排序算法/插入排序.js
@@ -1,21 +1,21 @@
-function insertionSort(arr){
-  const len = arr.length;
-  if(len<=1) return;
+/**
+ * 插入排序: 在接近有序的情况下，表现优异
+ * 基本思想：每次将一个数字插入一个有序的数组里，成为一个长度更长的有序数组，有限次操作以后，数组整体有序。初始已排序区间只有一个元素，就是数组的第一个元素。
+ * 分析：「插入排序」在「几乎有序」的数组上表现良好，特别地，在「短数组」上的表现也很好。因为「短数组」的特点是：每个元素离它最终排定的位置都不会太远。为此，在小区间内执行排序任务的时候，可以转向使用「插入排序」
+ */
+var sortArray = function(nums) {
+  const len = nums.length;
+  // 循环不变量：将 nums[i] 插入到区间 [0, i) 使之成为有序数组
   for(let i=1; i<len; i++){
-    const currentVal = arr[i];
-    let j = i-1;
-    // 查找插入的位置
-    for(; j>=0; j--){
-      if(arr[j] > currentVal){
-        arr[j+1] = arr[j]  // 移动数据
-      }else{
-        break;
+      // 先暂存这个元素，然后之前元素逐个后移，留出空位
+      const currentVal = nums[i];
+      let j = i;
+      // 注意边界 j > 0
+      while(j>0 && nums[j-1] > currentVal ){
+          nums[j] = nums[j-1];
+          j--;
       }
-    }
-    arr[j+1] = currentVal; // 插入数据
+      nums[j] = currentVal;
   }
-  return arr;
-}
-
-const arr = [4,5,6,1,3,3,2];
-console.log(insertionSort(arr))
+  return nums;
+};
